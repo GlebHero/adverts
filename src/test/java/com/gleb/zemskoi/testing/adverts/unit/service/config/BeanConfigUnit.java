4b632f2ec27faa146f90a5f381e5fb1cab258631
Gleb Zemskoi <glebzems@gmail.com>
2021-05-28 21:46:36 -0700

    fix test

--- src/test/java/com/gleb/zemskoi/testing/adverts/unit/service/config/BeanConfigUnit.java
+++ src/test/java/com/gleb/zemskoi/testing/adverts/unit/service/config/BeanConfigUnit.java
@@ -3,13 +3,16 @@ package com.gleb.zemskoi.testing.adverts.unit.service.config;
 import com.gleb.zemskoi.adverts.converter.AdvertConverter;
 import com.gleb.zemskoi.adverts.dao.AdvertRepository;
 import com.gleb.zemskoi.adverts.dao.CustomerRepository;
+import com.gleb.zemskoi.adverts.entity.common.CustomerInfo;
 import com.gleb.zemskoi.adverts.mq.Producer;
 import com.gleb.zemskoi.adverts.service.AdvertStopWordService;
+import com.gleb.zemskoi.adverts.service.security.JwtUserDetailsService;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootConfiguration
-@MockBean({AdvertRepository.class, CustomerRepository.class, AdvertConverter.class, Producer.class, AdvertStopWordService.class})
+@MockBean({AdvertRepository.class, CustomerRepository.class, AdvertConverter.class, Producer.class,
+        AdvertStopWordService.class, CustomerInfo.class, JwtUserDetailsService.class})
 public class BeanConfigUnit {
 
 }
